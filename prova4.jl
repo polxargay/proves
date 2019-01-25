b02875a866686dd7ec818e433573cdc0ca718091
Pol <polxargayiferrer@gmail.com>
2019-01-25 20:07:49 +0100

    dona error la línia 9 del codi, no em deixa assignar valors dins de l'estructura

--- prova4.jl
+++ prova4.jl
@@ -1,4 +1,11 @@
-struct serverA
+struct ServerA
     id::Int32
     name::Int32
 end
+
+servidorA = ServerA(3,4)
+println(ServerA(3,4))
+servidorA2= servidorA
+servidorA2.id=1
+println(servidorA)
+println(servidorA2)
