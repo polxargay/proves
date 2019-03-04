2fa4f9393f33a81a4080a5a4b7bf14199a8de368
Pol <polxargayiferrer@gmail.com>
2019-03-04 18:59:24 +0100

    prova amb malloc, però no funciona

--- prova1.jl
+++ prova1.jl
@@ -5,12 +5,19 @@ end
 
 
 function prov(num_nodes::Int64)
-no = Nodes[]
-    for x in 1:num_nodes
-        no[x] = Nodes(x,"ACK")
+    no = Nodes[2]
+    #size_t *ptr = malloc(num_nodes * sizeof(size_t))
+    #=for x in 1:num_nodes
+        #prt[x].id = 1
+        #prt[x].ack = "ACK"
+        no[x].id = 1
+        no[x].ack = "ACK"
+    end=#
+    #=for x in 1:num_nodes
+        no[x].id = 1
         #push!(no,(1,"ACK"))
         #println(no)
     end
-    println(no)
+    println(no)=#
 end
 prov(2)
