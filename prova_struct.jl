cb0a0142685834c7ef93f4d98e32e08e226f868d
Pol <polxargayiferrer@gmail.com>
2019-03-16 16:27:03 +0100

    creació un array d'structs

--- prova_struct.jl
+++ prova_struct.jl
@@ -1,21 +1,15 @@
-mutable struct Nodes_1
+struct Nodes_1
     id
     ack
 end
-nodes = []
-#println(x)
+#create an empty array to save all the nodes created
+nodes_array = []
+
+#create the constructor for the nodes
+Nodes_1(x) = Nodes_1(x,"hola")
+
+#fill the array with all the necessary nodes
 for i in 1:10
-    #push!(nodes,i)
-    nodes[i] = typeof()
+    push!(nodes_array,Nodes_1(i))
 end
-
-println(nodes)
-
-#node = Nodes_1([1,2],"Hello")
-#=node = Nodes_1(1,"ACK")
-println(typeof(node))
-fieldnames(typeof(node))
-node.id
-node.ack
-
-append!(node.id, 4)=#
+println(nodes_array[1])
