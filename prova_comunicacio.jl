968229cb9e6e0c4327a55a214b7a3fb0f2ad75dd
Pol <polxargayiferrer@gmail.com>
2019-03-18 19:42:51 +0100

    funciona bé l'input de dades

--- prova_comunicacio.jl
+++ prova_comunicacio.jl
@@ -4,30 +4,29 @@ struct EAXON
     EAXON(x) = new(x,"ACK")
 end
 
-function main_function()
-    #println("hello")
-    num_eaxon = num_eaxons()
-    println("Vols crear",num_eaxon," eaxons")
-    create_eaxons(num_eaxon)
-end
-
 #Create an empty array of EAXON struct
 eaxons = []
 
-#demano el número de eAXONs que vol crear
-#=print("Quants eAXONs vols crear ");num_eaxon = parse(UInt8, readline())
-println(num_eaxon)=#
-#num_eaxon = 10
+function main_function()
+    #println("hello")
+    num_eaxon = num_eaxons()
+    println("Vols crear ",num_eaxon," eaxons") #prova de debug
+    eaxons_array = create_eaxons(num_eaxon)
+    println(eaxons_array)
+end
 
+#Creo el número d'eAXONs necessaris
 function num_eaxons()
     print("Quants eAXONs vols crear ")
+    #la funció readline() retorna un String, per tant cal parsejar-lo com a Int
     num_eaxon = parse(Int,readline())
     return num_eaxon
 end
 
+#Assigno ID a cada un dels eAXONs
 function create_eaxons(num_eaxon)
     for x in 1:num_eaxon
         push!(eaxons,EAXON(x))
     end
-    println(eaxons)
+    return eaxons
 end
