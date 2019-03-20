798a328c5d19685ddb6be33da93754d71c9b6a78
Pol <polxargayiferrer@gmail.com>
2019-03-20 23:18:52 +0100

    prova de menú correcta

--- prova_comunicacio.jl
+++ prova_comunicacio.jl
@@ -12,14 +12,19 @@ function main_function()
     opc = -1
     while opc!=5
         while (opc<1||opc>5)
+            println("\n**********************************************************")
             println("\nSimulador per a microcontroladors injectables\n")
-            print("\n1 - Definir número d'eAXONs\n2 - Definir grup d'un eAXON\n3 - Comunicar-se amb un eAXON\n")
-
+            println("**********************************************************\n")
+            print("\n1 - Definir número d'eAXONs\n2 - Definir grup d'un eAXON\n3 - Comunicar-se amb un eAXON\n4 - Simular delay d'n transmissions\n5 - Sortir\nTria una opció: ")
+            opc = parse(Int,readline())
             if (opc == 1)
                 neaxon = num_eaxons()
-                println("Vols crear ",neaxon," eaxons") #prova de debug
+                println("\nHas creat ",neaxon," eaxons\n") #prova de debug
                 eaxons_array = create_eaxons(neaxon)
-                println(eaxons_array)
+                println("\nS'han creat els següents eAXONs: ",eaxons_array)
+                return main_function()
+            elseif (opc == 5)
+                #println("\n")
             else
                 opc = -1
             end
